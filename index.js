04e3244a1ed1afaf83f74b34d110e21568d945ed
nicholas koyi <koyinicholas@gmail.com>
2024-04-04 02:04:35 +0300

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Add your code here
+function submitData(userName, userEmail) {
+  return fetch('http://localhost:3000/users', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      Accept: 'application/json',
+    },
+    body: JSON.stringify({
+      name: userName,
+      email: userEmail,
+    }),
+  })
+    .then((response) => {
+      return response.json();
+    })
+    .then((data) => {
+      const body = document.querySelector('body');
+      const div = document.createElement('div');
+      div.innerHTML = data.id;
+      body.appendChild(div);
+    })
+    .catch((error) => {
+      const body = document.querySelector('body');
+      const div = document.createElement('div');
+      div.innerHTML = error.message;
+      body.appendChild(div);
+    });
+}
